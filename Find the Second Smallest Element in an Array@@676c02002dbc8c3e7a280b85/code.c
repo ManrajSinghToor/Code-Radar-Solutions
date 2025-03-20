f4f0c4bb6836fe2c3f7b400797bddce9d76194e6
Manraj Singh Toor <manrajtoorsingh@gmail.com>
2025-03-20 23:06:17 +0530

    Solution to Find the Second Smallest Element in an Array

--- Find the Second Smallest Element in an Array@@676c02002dbc8c3e7a280b85/code.c
+++ Find the Second Smallest Element in an Array@@676c02002dbc8c3e7a280b85/code.c
@@ -1,9 +1,8 @@
-// Your code here...
 #include<stdio.h>
-int sort(int arr[],int n){
+int check(int arr[],int n){
     for(int i=0;i<n-1;i++){
         for(int j=0;j<n-i-1;j++){
-            if(arr[j]<arr[j+1]){
+            if(arr[j] > arr[j+1]){
                 int new = arr[j];
                 arr[j] = arr[j+1];
                 arr[j+1] = new;
@@ -11,37 +10,27 @@ int sort(int arr[],int n){
         }
     }
 }
+
 int main(){
     int n;
-    scanf("%d\n",&n);
+    scanf("%d",&n);
     int arr[n];
-    for(int i=0;i<=n;i++){
-        scanf("%d ",&arr[i]);
+    for(int i=0;i<n;i++){
+        scanf("%d",&arr[i]);
     }
-    sort(arr, n);
-    if (n > 2 && arr[n - 1] == arr[n - 2]){
-        printf("%d",arr[n-3]);
+    check(arr,n);
+    int first = arr[0];
+    int second_found = 0;
+    
+    for (int i = 1; i < n; i++) {
+        if (arr[i] != first) {
+            printf("%d\n", arr[i]);
+            second_found = 1;
+            break;
+        }
     }
-    if (n > 1 && arr[n - 1] != arr[n - 2]) {
-        printf("%d", arr[n - 2]);
-    } 
-    else{
-        printf("-1");
+    if (!second_found) {
+        printf("-1\n");
     }
     return 0;
 }
-    // for(int i=0;i<n;i++){
-    // if(sort(arr,n) && (arr[i]!=1)){
-    //     printf("%d ",arr[n-2]);
-    //     break;
-    // }
-    // if(sort(arr,n) && (arr[i] = 1)){
-    //     printf("%d ",arr[n-3]);
-    //     break;
-    // }
-//     else{
-//         printf("-1");
-//         break;
-//     }
-//   }
-// }
