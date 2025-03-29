33d3cb060fdd215c811231665de52bbd7ca8f04d
Manraj Singh Toor <manrajtoorsingh@gmail.com>
2025-03-29 23:18:45 +0530

    Solution to Fibonacci Series

--- Fibonacci Series@@67ca7acd1fe08d7207c253c6/code.c
+++ Fibonacci Series@@67ca7acd1fe08d7207c253c6/code.c
@@ -3,6 +3,7 @@ int fibonacciSeries(int n) {
     if(n<=2){
         return 1;
     }int fib = fibonacciSeries(n-1)+fibonacciSeries(n-2);
-    printf("%d",fib);
+    int fibn = fib;
+    printf("%d ",fibn);
 }
 
